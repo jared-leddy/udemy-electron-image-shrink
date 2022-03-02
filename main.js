eaa08760124c163e8b9597f2ac30c3779ce8ee92
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-03-02 00:43:28 -0500

    Update window loader

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 // npm modules
 const { app, BrowserWindow } = require('electron');
 
+let mainWindow;
+
 function createMainWindow() {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     title: 'Image Shrink',
     width: 500,
     height: 600,
   });
+
+  // protocols required
+  //   mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+
+  // protocools not required
+  mainWindow.loadFile('./app/index.html');
 }
 
 app.on('ready', createMainWindow);
