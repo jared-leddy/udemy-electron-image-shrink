d7c11345a65f141f8ffd601afa2e1c02efdb7937
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-03-02 00:34:37 -0500

    setup base and create main window

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+// npm modules
+const { app, BrowserWindow } = require('electron');
+
+function createMainWindow() {
+  const mainWindow = new BrowserWindow({
+    title: 'Image Shrink',
+    width: 500,
+    height: 600,
+  });
+}
+
+app.on('ready', createMainWindow);
