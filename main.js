587e9f4f3be8909c7d63f9dffaa0e6b53c52e04e
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-03-02 00:47:30 -0500

    Add app icon

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ function createMainWindow() {
     title: 'Image Shrink',
     width: 500,
     height: 600,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
   });
 
   // protocols required
